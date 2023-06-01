f50140c22b358dcd198feb34abf732f04f1c5ea3
zhuangjiaju <zhuangjiaju@users.noreply.github.com>
2023-06-01 01:27:53 +0000

    修改关键字

---
+++ assets/js/3343.dde5b045.js
@@ -0,0 +1,34 @@
+"use strict";
+exports.id = 3343;
+exports.ids = [3343];
+exports.modules = {
+
+/***/ 83343:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   dyte_ai_chat: () => (/* binding */ DyteAiChat)
+/* harmony export */ });
+/* harmony import */ var _index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(67503);
+
+
+const dyteAiChatCss = ":host{line-height:initial;font-family:var(--dyte-font-family, sans-serif);font-feature-settings:normal;font-variation-settings:normal}p{margin:var(--dyte-space-0, 0px);padding:var(--dyte-space-0, 0px)}:host{display:flex;height:100%;width:100%;flex-direction:column}.private-message{text-align:center;font-size:12px;color:rgb(var(--dyte-colors-text-600, 255 255 255 / 0.52));margin-top:var(--dyte-space-8, 32px);margin-bottom:var(--dyte-space-8, 32px);margin-left:var(--dyte-space-10, 40px);margin-right:var(--dyte-space-10, 40px)}.hint-message{display:flex;flex-direction:column;align-items:center;justify-content:center;text-align:center;flex:1 1 0%;font-size:14px;line-height:1.5;color:rgb(var(--dyte-colors-text-700, 255 255 255 / 0.64))}i{font-weight:500;font-style:italic;color:rgb(var(--dyte-colors-text-900, 255 255 255 / 0.88))}";
+
+const DyteAiChat = class {
+  constructor(hostRef) {
+    (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.r)(this, hostRef);
+  }
+  render() {
+    return ((0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)(_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.H, null, (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("p", { class: "private-message" }, "This conversation will just be visible to you and not to others in the call."), (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("div", { class: "hint-message" }, (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("p", null, "Ask ", (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("i", null, "\"Hey AI, summarise this call\""), (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("br", null), " or ", (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("br", null), "Type ", (0,_index_fe685ee5_js__WEBPACK_IMPORTED_MODULE_0__.h)("i", null, "\"Hey AI, what is today's agenda?\"")))));
+  }
+};
+DyteAiChat.style = dyteAiChatCss;
+
+
+
+
+/***/ })
+
+};
+;
